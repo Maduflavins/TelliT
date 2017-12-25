f171c0c7e521f0f48ebada6ae4a2e53f9e3be3c8
Flavins <maduabuchiokonkwo@gmail.com>
2017-12-25 16:39:08 +0100

    set up core commands

---
+++ server.js
@@ -0,0 +1,22 @@
+const express =     require('express'),
+morgan        =     require('morgan'),
+bodyParser    =     require('body-parser'),
+mongoose      =     require('mongoose'),
+hbs           =     require('hbs'),
+expressHbs    =     require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, (err) =>{
+    if(err) console.log(err);
+    console.log('Running on port ${3000}')
+});
+
