d5789f1b1d3b13cca2be49e10fd2c50923d530c6
Flavins <maduabuchiokonkwo@gmail.com>
2017-12-25 18:50:35 +0100

    create layout pages and install bootstrap and cosmo bootstrap

---
+++ routes/main.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+
+router.get('/', (req, res, next)=>{
+    res.render('main/landing',);
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,17 @@ expressHbs    =     require('express-handlebars');
 
 const app = express();
 
-app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const mainRoutes = require('./routes/main');
+
+app.use(mainRoutes);
 
 
 app.listen(3000, (err) =>{
